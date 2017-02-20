46e554e1888652cb68e192414430da05366c6c55
Carlo Strub <cs@carlostrub.ch>
2017-02-20 22:57:33 +0000

    get lists of mail keys

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"github.com/spf13/viper"
+	"fmt"
+	"log"
+
+	"github.com/luksen/maildir"
+)
+
+var (
+	// Maildirs holds a set of mail directories to handle.
+	Maildirs []string
 )
 
 func main() {
-	viper.SetConfigName("sisyphus")
+	Maildirs = []string{"/usr/home/cs/Maildir.TEST"}
+
+	var err error
+	var Bad, Good []string
+
+	for _, dir := range Maildirs {
+		var keysBad, keysGood []string
+		keysBad, err = maildir.Dir(dir + "/.Junk").Keys()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		Bad = append(Bad, keysBad...)
+
+		keysGood, err = maildir.Dir(dir).Keys()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		Good = append(Good, keysGood...)
+	}
+
+	fmt.Println("Bad guys:")
+	fmt.Println(Bad)
+	fmt.Println("Good guys:")
+	fmt.Println(Good)
 }
