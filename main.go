7208399fac46f762918108fcdb53a211473b442a
Carlo Strub <cs@carlostrub.ch>
2017-03-11 20:34:54 +0000

    use cli to create a decent command line app

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"flag"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/urfave/cli"
 )
 
 var (
@@ -13,29 +15,66 @@ var (
 )
 
 func main() {
-
-	// Get the Maildir to be handled and the DB
+	// Get working directory
 	wd, err := os.Getwd()
 	if err != nil {
 		return log.Fatalf("get working directory: %s", err)
 	}
-	maildir := flag.String("maildir", wd+"/Maildir", "Path to the Maildir")
-	database := flag.String("database", wd+"/sisyphus.db", "Path to the sisyphus database")
-	flag.Parse()
 
-	// Load the Maildir
-	mails, err := Index(*maildir)
-	if err != nil {
-		return log.Fatalf("load Maildir content: %s", err)
+	var maildir, database *string
+
+	// Define App
+	app := cli.NewApp()
+	app.Name = "sisyphus"
+	app.HelpName = "Intelligent Junk and Spam Mail Handler"
+	app.Version = "0.0.0"
+	app.Authors = []Author{
+		Author{
+			Name:  "Carlo Strub",
+			Email: "cs@carlostrub.ch",
+		},
 	}
 
-	fmt.Println(mails)
-
-	// Open the database
-	db, err := openDB("sisyphus.db", 0600, nil)
-	if err != nil {
-		return log.Fatalf("open database: %s", err)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "maildir",
+			Value:       wd + "/Maildir",
+			Usage:       "Path to the Maildir directory",
+			Destination: &maildir,
+		},
+		cli.StringFlag{
+			Name:        "database",
+			Value:       wd + "/sisyphus.db",
+			Usage:       "Path to the sisyphus database",
+			Destination: &database,
+		},
 	}
-	defer db.Close()
 
+	app.Action = func(c *cli.Context) error {
+		if database == nil {
+			return errors.New("no database selected")
+		}
+		if maildir == nil {
+			return errors.New("no maildir selected")
+		}
+
+		// Load the Maildir
+		mails, err := Index(*maildir)
+		if err != nil {
+			return log.Fatalf("load Maildir content: %s", err)
+		}
+
+		fmt.Println(mails)
+
+		// Open the database
+		db, err := openDB(*database, 0600, nil)
+		if err != nil {
+			return log.Fatalf("open database: %s", err)
+		}
+		defer db.Close()
+
+		return nil
+	}
+
+	app.Run(os.Args)
 }
