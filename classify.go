9376143894ecb791386610bf4b4399af5bc05c12
Carlo Strub <cs@carlostrub.ch>
2018-04-22 21:36:57 +0200

    please gometalinter

--- classify.go
+++ classify.go
@@ -218,7 +218,7 @@ func Junk(db *bolt.DB, wordlist []string) (junk bool, prob float64, err error) {
 		}
 
 		var wordlistTempSlice []string
-		for key, _ := range wordlistTemp {
+		for key := range wordlistTemp {
 			wordlistTempSlice = append(wordlistTempSlice, key)
 		}
 		wordlist = wordlistTempSlice
